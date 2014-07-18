a864decee6f909f58de55714d03ec8b86b3a5ae6
Molly Huerster <mary.huerster@gmail.com>
2014-07-18 16:42:50 -0400

    working comment method - doesn't display anywhere yet

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,8 +1,9 @@
 get '/comment_on_line' do
-	@line = line
+	# @line = line
 	erb :comment_on_line
 end
 
 post '/comment_on_line' do
-	Comment.create(text: params[:content], line: params[:line_id], user: @user.id)
+	Comment.create(text: params[:user_comment], line_id: params[:line_id], user_id: session[:user_id])
 	redirect '/comment_on_line'
+end
