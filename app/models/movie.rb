5ed64f564acf1781ec20fc28c321e02b990ca119
AviFoxi <frosenox@gmail.com>
2014-07-18 17:24:57 -0400

    rework movie creation to auto create lines

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,17 @@
 class Movie < ActiveRecord::Base
   # Remember to create a migration!
   has_many :lines
+
+  before_save :line_creator
+	
+
+  private
+
+  def line_creator
+  	self.synopsis.split(".").each do |line| 
+  		self.lines << Line.create(text: line)
+  	end
+  end
+
 end
+
