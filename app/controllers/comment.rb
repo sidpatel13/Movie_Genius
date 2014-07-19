0d9e2ad7d158d8fca98387c409735ee9cc84f170
Sid Patel <sidpatel13@gmail.com>
2014-07-19 15:02:10 -0400

    you can now comment on specific lines, redirect route works and takes you back to the line

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,9 +1,10 @@
 get '/comment_on_line/:id' do
 	@line = Line.find(params[:id])
+	@line.comments
 	erb :comment_on_line
 end
 
 post '/comment_on_line' do
-	Comment.create(text: params[:user_comment], line_id: params[:line_id], user_id: session[:user_id])
-	redirect '/comment_on_line'
+	comment = Comment.create(text: params[:user_comment], line_id: params[:line_id], user_id: session[:user_id])
+	redirect "/comment_on_line/#{comment.line.id}"
 end
