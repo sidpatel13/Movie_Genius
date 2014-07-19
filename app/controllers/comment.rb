c779cf50435154670c3cc9f71f71636193564f9c
Sid Patel <sidpatel13@gmail.com>
2014-07-19 16:52:57 -0400

    created voting page, voting functionality through jquery to come, Raleway font throughout pages

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -8,3 +8,13 @@ post '/comment_on_line' do
 	comment = Comment.create(text: params[:user_comment], line_id: params[:line_id], user_id: session[:user_id])
 	redirect "/comment_on_line/#{comment.line.id}"
 end
+
+
+get '/comments/:id' do
+	@comment = Comment.find(params[:id])
+	@movie = @comment.line.movie
+
+
+
+  erb :specific_comment
+end
