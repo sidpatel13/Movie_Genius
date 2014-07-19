2145d15d0639e835d0257d6ac44f35aa6dab6164
AviFoxi <frosenox@gmail.com>
2014-07-19 16:05:06 -0400

    expand seed file to include comments and users, add top comments instance var to root controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
+require 'faker'
+
 meh = User.create(username:"molly", password:"1234q")
 
+### generate Random Users for Diverse Comments
 
+100.times do
+	User.create(username: Faker::Name.name, password: (rand(10).to_s + Faker::Lorem.word + rand(10).to_s))
+end
 
 # user = User.create(username: "sidpatel", password: "password")
 # user.comments.create(text: "yo what it is" )
 
 
 a = Imdb::Movie.new("0075314") # Taxi Driver
-
 a_movie = Movie.create(title: a.title, synopsis: a.plot_synopsis)
-
-
-
-
 
 
 b = Imdb::Movie.new("0119177") # Gattaca
@@ -49,6 +50,22 @@ i_movie = Movie.create(title: i.title, synopsis: i.plot_synopsis)
 
 
 j = Imdb::Movie.new("0133093") #The Matrix
-
 j_movie = Movie.create(title: j.title, synopsis: j.plot_synopsis)
+
+
+##### SEED COMMENTS
+
+100.times do 
+	Comment.create(text: Faker::Lorem.sentence, vote_count: (rand(100)- 50), line_id: rand(1250), user_id: rand(100)) 
+end
+
+
+
+
+
+
+
+
+
+
 
