26c809145be189f9195b970bc3c300f335679930
Molly Huerster <mary.huerster@gmail.com>
2014-07-20 12:52:52 -0400

    start adding search functionality
    create movie search controller, search form and results page - working, but imperfectly

---
+++ app/controllers/movie.rb
@@ -0,0 +1,12 @@
+get '/movie/add' do
+  erb :add_movie
+end
+
+post '/find_movie' do
+  redirect "/search_results/#{params[:user_movie]}"
+end
+
+get '/search_results/:search' do
+  @user_movie = Imdb::Search.new(params[:search])
+  erb :search_results
+end
